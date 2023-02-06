0d1e5ca9c4329a4c9afe10b5a65bc962aa6c6cc8
ErickDC <erickdiazc717@gmail.com>
2023-02-06 17:11:14 -0500

    view.js file created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages";
 import Create from "./pages/create";
+import View from "./pages/view";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/create" element={<Create />} />
+          <Route path="/view/:bookId" element={<View />} />
         </Routes>
       </BrowserRouter>
     </div>
